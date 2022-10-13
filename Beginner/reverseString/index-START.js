0925e8c22498af00e50b36bc87e4b48f7c14e64a
brandon <brandon.l.merry@gmail.com>
2022-10-13 12:14:37 +0800

    created functions for string reversal

--- Beginner/reverseString/index-START.js
+++ Beginner/reverseString/index-START.js
@@ -3,12 +3,52 @@ Given a string of text, write an algorithm that returns the text received in a r
 E.g reverseString('algorithms') // should return 'smhtirogla'
 */
 
-
+// CHAINING BUILT-IN METHODS
 
 function reverseString(text) {
-    // Code goes here
+  return text.split('').reverse().join('');
 }
 
+//  CHAINING BUILT-IN METHODS USING ES6
 
+function reverseString(text) {
+  return [...text].reverse().join('');
+}
 
-module.exports = reverseString
+// USING A FOR LOOP
+
+function reverseString(text) {
+  let result = '';
+  for (let i = text.length - 1; i >= 0; i--) {
+    result += text[i];
+  }
+  return result;
+}
+
+// USING A FOR..OF LOOP IN ES6
+
+function reverseString(text) {
+  let result = '';
+  for (let char of text) {
+    result = char + result;
+  }
+  return result;
+}
+
+// RECURSIVE METHOD
+
+function reverseString(text) {
+  if (text === '') {
+    return '';
+  } else {
+    return reverseString(text.substr(1)) + text[0];
+  }
+}
+
+// USING .REDUCE()
+
+function reverseString(text) {
+  return text.split('').reduce((acc, char) => char + acc);
+}
+
+module.exports = reverseString;
