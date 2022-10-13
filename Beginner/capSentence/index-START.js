374a7f2f4c028dcfe29edc046f28e489d577bb56
brandon <brandon.l.merry@gmail.com>
2022-10-13 13:11:39 +0800

    .map().slice() 2 cap frst letter of word in string

--- Beginner/capSentence/index-START.js
+++ Beginner/capSentence/index-START.js
@@ -4,13 +4,36 @@ return the equivalent of the sentence when capitalised. E.g
   capSentence('the tales of scotch!') // would return 'The Tales Of Scotch!' 
 */
 
-
-
-
+// USE .forEach()
 function capSentence(text) {
-   // Code goes here
+  let wordsArray = text.toLowerCase().split(' ');
+  let capsArray = [];
+
+  wordsArray.forEach((word) => {
+    capsArray.push(word[0].toUpperCase() + word.slice(1));
+  });
+
+  return capsArray.join(' ');
 }
 
+//USE .map() & .splice()
+function capSentence(text) {
+  let wordsArray = text.toLowerCase().split(' ');
+  let capsArray = wordsArray.map((word) => {
+    return word[0].toUpperCase() + word.slice(1);
+  });
+  return capsArray.join(' ');
+}
 
+//USE .map() & .replace()
+function capSentence(text) {
+  let wordsArray = text.toLowerCase().split(' ');
 
-module.exports = capSentence
+  let capsArray = wordsArray.map((word) => {
+    return word.replace(word[0], word[0].toUpperCase());
+  });
+
+  return capsArray.join(' ');
+}
+
+module.exports = capSentence;
